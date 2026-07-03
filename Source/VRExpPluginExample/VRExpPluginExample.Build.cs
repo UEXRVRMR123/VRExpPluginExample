5b6311c9b8d133dd966d02fc402878266cd124c6
zsl <zsl@qq.com>
2026-07-03 17:34:11 +0800

    手势追踪微调

--- Source/VRExpPluginExample/VRExpPluginExample.Build.cs
+++ Source/VRExpPluginExample/VRExpPluginExample.Build.cs
@@ -12,9 +12,7 @@ public class VRExpPluginExample : ModuleRules
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
        // PrivatePCHHeaderFile = "Private/WindowsMixedRealityPrecompiled.h";
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AdvancedSessions", "VRExpansionPlugin", "PICOOpenXRHandTracking", "HeadMountedDisplay" });
-
-		PrivateDependencyModuleNames.AddRange(new string[] { "XRBase" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AdvancedSessions" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
